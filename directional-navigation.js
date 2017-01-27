44ad8fa8bf724e75e37a7b200eb00b07445199d1
Luis <luiscaceresmx@gmail.com>
2017-01-27 16:24:17 +1100

    Found a better algorithm for directional navigation. Code size has decreased in directional-navigation.js

--- directional-navigation.js
+++ directional-navigation.js
@@ -11,31 +11,16 @@ code identifies such widgets and enables directional navigation. */
 
         if (allowsDirectionalNavigation(activeElement)) {
             // at this point the currently focused element may have a sibling to navigate to. 
-            // The program has to itarate over the siblings to verify this assertion.
-            siblings = new Iterator(activeElement.parentElement.children);
-            siblings.autoreset = true;
-            siblings.find(activeElement);
-
-            let sibling;
-            while (sibling = siblings.next()) {
-
-                if (allowsDirectionalNavigation(sibling)) {
-                    // at this point it is clear there is a sibling to navigate to.
-                    activeElement.addEventListener('blur', blurHandler);
-                    activeElement.addEventListener('keydown', keydownHandler);
-                    break;
-                }
-            }
+            activeElement.addEventListener('blur', blurHandler);
+            activeElement.addEventListener('keydown', keydownHandler);
         }
     }, true);
+
 
     // identifies a composite widget requiring directional navigation
     function allowsDirectionalNavigation(element) {    
         return element.hasAttribute('tabindex') ? true : false;
     };
-
-
-    var siblings = null;
 
 
     var blurHandler = function blurHandler(event) {
@@ -47,24 +32,27 @@ code identifies such widgets and enables directional navigation. */
 
     // navigates forward or backwards to a focusable (if any) 
     var keydownHandler = function keydownHandler(event) {
+        var activeElement = document.activeElement;
+
         var keyCode = event.keyCode,
             // key codes correspond to the left, up, right and down arrow keys
             keyCodes = [37, 38, 39, 40];
 
         if (keyCodes.includes(keyCode)) {
             let direction = keyCode === 37 || keyCode === 38 ? 'previous' : 'next';
-
-            // starts iterating from the currently focused element
-            siblings.find(event.target);
+          
+            // at this point the currently focused element may have a sibling to navigate to. 
+            // The program has to itarate over the siblings to verify this assertion.
+            let siblings = new Iterator(activeElement.parentElement.children);
+            siblings.autoreset = true;
+            siblings.find(activeElement);
 
             let sibling;
             while (sibling = siblings[direction]()) {
 
                 if (allowsDirectionalNavigation(sibling)) {
                     // at this point the program has found a sibling to navigate to
-                    sibling.addEventListener('blur', blurHandler);
-                    sibling.addEventListener('keydown', keydownHandler);
-                    setTimeout(function(){
+                    setTimeout(function(){           
                         sibling.focus();
                     }, 0);
                     break;
