d5bab23ad2bbc65a8053ec4f16bc82e735d2be15
Luis <luiscaceresmx@gmail.com>
2017-02-02 01:36:46 +1100

    Enabled directional navigation through the END and HOME keys. Modified comments.

--- directional-navigation.js
+++ directional-navigation.js
@@ -1,27 +1,23 @@
-/* A composite widget may contain navigable descendants or owned children. To move 
+/* A composite widget may contain navigable descendants. To move 
 through the navigable descendants the user may press the HOME, END, or arrow keys.
 This is referred to as directional navigation. The following piece of code enables 
-directional navigation when required. */
+directional navigation for a composite widget. */
 
 (function () {
     'use strict';
 
-    // identifies a composite widget requiring directional navigation 
+    // attempts to detect a composite widget requiring directional navigation 
     document.addEventListener('focus', function directionalNavigationDetector(event) {
         var activeElement = document.activeElement;
 
-        if (allowsDirectionalNavigation(activeElement)) {
-            // at this point the currently focused element may have a sibling to navigate to. 
+        if (activeElement.hasAttribute('tabindex')) {
+            // assumes the element with focus belongs to a composite widget. This element
+            // may have siblings to navigate to. The application will respond to
+            // key presses while the element remains with focus.
             activeElement.addEventListener('blur', blurHandler);
             activeElement.addEventListener('keydown', keydownHandler);
         }
     }, true);
-
-
-    // identifies a composite widget requiring directional navigation
-    function allowsDirectionalNavigation(element) {    
-        return element.hasAttribute('tabindex') ? true : false;
-    };
 
 
     var blurHandler = function blurHandler(event) {
@@ -31,34 +27,60 @@ directional navigation when required. */
     }
 
 
-    // navigates forward or backwards to a focusable (if any) 
     var keydownHandler = function keydownHandler(event) {
-        var activeElement = document.activeElement;
+        var target = event.target;
 
-        var keyCode = event.keyCode,
-            // key codes correspond to the left, up, right and down arrow keys
-            keyCodes = [37, 38, 39, 40];
+        var keyPressed = event.keyCode,
+            // key codes map to the END, HOME and arrow keys
+            keys = [35, 36, 37, 38, 39, 40];
 
-        if (keyCodes.includes(keyCode)) {
-            let direction = keyCode === 37 || keyCode === 38 ? 'previous' : 'next';
-          
-            // at this point the currently focused element may have a sibling to navigate to. 
-            // The program has to itarate over the siblings to verify this assertion.
-            let siblings = new Iterator(activeElement.parentElement.children);
-            siblings.autoreset = true;
-            siblings.find(activeElement);
+        if (keys.includes(keyPressed)) {
+            // the siblings (if any) of the element with focus will be traversed to 
+            // find another focusable. 
+            let siblings = new Iterator(target.parentElement.children),
+                sibling,
+                method;  
 
-            let sibling;
-            while (sibling = siblings[direction]()) {
-
-                if (allowsDirectionalNavigation(sibling)) {
-                    // at this point the program has found a sibling to navigate to
-                    setTimeout(function(){           
-                        sibling.focus();
-                    }, 0);
-                    break;
-                }
+            if (keyPressed === 35) {
+                // END key was pressed
+                // user wishes to navigate to the very last focusable
+                // sets a starting point for traversal
+                sibling = siblings.last();
+                method = 'previous';
             }
+
+            else if (keyPressed == 36) {
+                // HOME key was pressed
+                // user wishes to navigate to the very first focusable
+                // sets a starting point for traversal
+                sibling = siblings.first();
+                method = 'next';
+            }
+
+            else {
+                // an arrow key was pressed
+                // user wishes to navigate forward or backwards to another focusable
+                siblings.find(target);
+                siblings.autoreset = true;
+                method = keyPressed === 37 || keyPressed === 38 ? 'previous' : 'next';         
+                // sets a starting point for traversal
+                sibling = siblings[method]();
+            }
+         
+            // if 'sibling' is not focusable already, it iterates over the other siblings 
+            // until a focusable is found
+            while (sibling.hasAttribute('tabindex') === false) {
+                sibling = siblings[method]();
+            }
+
+            // at this point a focusable sibling has been found. It will gain focus soon.
+            setTimeout(function () {
+                // swaps the values of the 'tabindex' attribute. A composite widget can 
+                // contain only one element with a 'tabindex' value of 0.
+                target.tabIndex = -1;
+                sibling.tabIndex = 0;
+                sibling.focus();
+            }, 0);
 
             event.preventDefault();
             event.stopPropagation();
