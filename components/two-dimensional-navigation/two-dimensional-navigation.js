35a2f5a26139b00db6b9090550446e39380e2a07
Luis <luiscaceresmx@gmail.com>
2017-02-13 23:31:28 +1100

    Decoupled the END and HOME functionality from the two dimensional grid navigation.

--- components/two-dimensional-navigation/two-dimensional-navigation.js
+++ components/two-dimensional-navigation/two-dimensional-navigation.js
@@ -1,3 +1,6 @@
+if (typeof window === 'undefined') require('./../../internet browser host environment.js');
+
+
 /* Two dimensional navigation allows the user to move focus between cells in a grid. 
 A grid is an interactive control which contains cells of tabular data arranged in 
 rows and columns, like a table. The following piece of code implements two dimensional 
@@ -18,8 +21,6 @@ navigation for a grid. */
     }, true);
 
 
-    const END = 35;
-    const HOME = 36;
     const LEFT = 37;
     const UP = 38;
     const RIGHT = 39;
@@ -38,7 +39,7 @@ navigation for a grid. */
         var activeCell = event.target;
 
         var keyPressed = event.keyCode,
-            keys = [END, HOME, LEFT, UP, RIGHT, DOWN];
+            keys = [LEFT, UP, RIGHT, DOWN];
 
         if (keys.includes(keyPressed) === false) {
             return;
@@ -47,55 +48,45 @@ navigation for a grid. */
         let cells = new Iterator(activeCell.parentElement.querySelectorAll('[role=gridcell]')),
             cell;
 
-        if (keyPressed === END) {
-            cell = cells.last();
-        }
+        cells.positionAt(activeCell);
+        let method = keyPressed === LEFT || keyPressed === UP ? 'previous' : 'next';
+        cell = cells[method]();
 
-        else if (keyPressed === HOME) {
-            cell = cells.first();
-        }
+        activeCell = Rectangle.from(activeCell);
 
-        else {
-            cells.find(activeCell);
-            let method = keyPressed === LEFT || keyPressed === UP ? 'previous' : 'next';
+        // Iterates over the cells (in the direction specified by the arrow key pressed)
+        while (cell) {
+            cell = Rectangle.from(cell);
+            let location = cell.locationFrom(activeCell);
+
+            if ((location === 2 && keyPressed === UP) ||
+                (location === 8 && keyPressed === DOWN) ||
+                (location === 4 && keyPressed === LEFT) ||
+                (location === 6 && keyPressed === RIGHT)) {
+                cell = cells.current();
+                activeCell = event.activeCell;
+                break;
+            }
+
+            // Tries with another cell until an adjacent cell is found or there are no more cells.
             cell = cells[method]();
+        }
 
-            activeCell = Rectangle.from(activeCell);
-
-            // Iterates over the cells (in the direction specified by the arrow key pressed)
-            while (cell) {
-                cell = Rectangle.from(cell);
-                let location = cell.locationFrom(activeCell);
-
-                if ((location === 2 && keyPressed === UP) ||
-                    (location === 8 && keyPressed === DOWN) ||
-                    (location === 4 && keyPressed === LEFT) ||
-                    (location === 6 && keyPressed === RIGHT)) {
-                    cell = cells.current();
-                    activeCell = event.activeCell;
-                    break;
-                }
-
-                // Tries with another cell until an adjacent cell is found or there are no more cells.
-                cell = cells[method]();
-            }
-
-            if (!cell) {
-                // Realizes there's no more cells in the direction specified by the arrow key pressed.
-                // This is because the active cell is one of the outermost ones in the grid.
-                console.log('No more cells in this direction! User may need to be alerted');
-                cell = event.activeCell;
-            }
-            else {
-                // realizes an adjacent cell has been found. It will gain focus soon.
-                setTimeout(function () {
-                    // swaps the values of the 'tabindex' attribute. A composite widget can 
-                    // contain only one element with a 'tabindex' value of 0.
-                    event.target.tabIndex = -1;
-                    cell.tabIndex = 0;
-                    cell.focus();
-                }, 0);
-            }
+        if (!cell) {
+            // Realizes there's no more cells in the direction specified by the arrow key pressed.
+            // This is because the active cell is one of the outermost ones in the grid.
+            console.log('No more cells in this direction! User may need to be alerted');
+            cell = event.activeCell;
+        }
+        else {
+            // realizes an adjacent cell has been found. It will gain focus soon.
+            setTimeout(function () {
+                // swaps the values of the 'tabindex' attribute. A composite widget can 
+                // contain only one element with a 'tabindex' value of 0.
+                event.target.tabIndex = -1;
+                cell.tabIndex = 0;
+                cell.focus();
+            }, 0);
         }
 
         event.preventDefault();
