f337509636925d1c432a0564c329ac913261f165
Luis <luiscaceresmx@gmail.com>
2017-02-13 23:15:10 +1100

    Added JSDoc comments

--- components/Class Iterator/Class Iterator.js
+++ components/Class Iterator/Class Iterator.js
@@ -1,64 +1,80 @@
-/* An instance of Iterator is an iterator over the members of an indexed collection.
-The following is the implementation. */
+/* The Iterator interface represents an iterator over the members of a list of  
+data. The data is ordered by an index value. This includes arrays and array-like
+constructs such as Array objects and TypedArray objects. */
 
 var Iterator = (function () {
     "use strict";
 
-    // constructs an iterator. 'iterable' must be an indexed collection 
+    /**
+      * Creates an iterator over the members of 'iterable'. 
+      * @param iterable An array or array-like object.
+      * @returns An iterator.
+      */
     function Iterator(iterable /*, order*/) {
         this.order = arguments[1];
         this.index = -1;
         this.iterable = iterable;
     };
 
-    var proto = Iterator.prototype;
+
+    var prototype = Iterator.prototype;
 
 
-    // indicates whether the iterator must be automatically moved to the first or last
-    // position once 'iterable' has been exhausted
-    proto.autoreset = false;
+    // Indicates if the iterator should be moved to the first or last position once 
+    // the iterator has reached the beginning or the end of the list.
+    prototype.autoreset = false;
 
 
-    // returns the element referenced by the iterator
-    proto.current = function current() {
+    /**
+      * @returns The value located in the current position of the iterator.
+      */
+    prototype.current = function current() {
         return this.iterable[this.index];
     };
 
 
-    // moves the iterator to the position occupied by 'value' if it is present in 'iterable'.
-    // returns 'true' if 'value' is found otherwise 'false' 
-    proto.find = function find(value) {
-        // 'indexOf' is accessed from 'Array.prototype' since 'iterable' may not have access 
-        // to this method through the prototype chain.
+    /**
+      * Moves the iterator to the position occupied by 'value' as long as 'value' is present in the list. 
+      * @param value The value to search for in the list.
+      * @returns true if 'value' was found otherwise false.
+      */
+    prototype.positionAt = function positionAt(value) {
         var index = Array.prototype.indexOf.call(this.iterable, value);
 
         if (index > -1) {
             this.index = index;
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     };
 
 
-    // moves the iterator to the first position and returns the referenced element
-    proto.first = function first() {
+    /**
+      * Moves the iterator to the first position in the list. 
+      * @returns The value located in the first position in the list.
+      */
+    prototype.first = function first() {
         this.index = 0;
         return this.iterable[this.index];
     };
 
 
-    // moves the iterator to the last position and returns the referenced element
-    proto.last = function last() {
+    /**
+      * Moves the iterator to the last position in the list. 
+      * @returns The value located in the last position in the list.
+      */
+    prototype.last = function last() {
         var iterable = this.iterable;
         this.index = iterable.length - 1;
         return iterable[this.index];
     };
 
 
-    // moves the iterator one position forward and returns the referenced element
-    proto.next = function (value) {
+     /**
+      * Moves the iterator one position forward in the list. 
+      * @returns The value located in that new position of the iterator.
+      */
+    prototype.next = function (value) {
         var index = ++this.index,
             iterable = this.iterable,
             order = this.order,
@@ -76,8 +92,11 @@ var Iterator = (function () {
     };
 
 
-    // moves the iterator one position backwards and returns the referenced element
-    proto.previous = function () {
+    /**
+      * Moves the iterator one position backwards in the list. 
+      * @returns The value located in that new position of the iterator.
+      */
+    prototype.previous = function () {
         var index = this.index <= this.iterable.length ? --this.index : this.iterable.length - 1,
             iterable = this.iterable,
             order = this.order,
@@ -96,3 +115,5 @@ var Iterator = (function () {
 
     return Iterator;
 })();
+
+if (typeof global !== 'undefined') global.Iterator = Iterator;
