d9ef6cc0df7d17ecf975b4aa3c19da2d56678460
Luis Caceres <luiscaceresmx@gmail.com>
2017-04-12 17:04:10 +1000

    Update screen-reader.js
    Added another case that deals with the pattern attribute.

--- components/screen-reader.js
+++ components/screen-reader.js
@@ -21,3 +21,15 @@ Tested on JAWS 17 on Internet Explorer 11
     }
   }, true);
 }());
+
+
+
+
+/* The 'pattern' attribute of an <input> element has no effect when the user is prevented from entering any characters. 
+In other words, a screen reader will not make the user aware of this situation. 
+For example, if an <input> expects only letters and the user presses the '9' key, the screen reader will say 'nine'. 
+However, no number '9' is inserted. This is frustrating for the user experience.
+This is especially true when no error message is displayed to the sighted user (presumably because it is very obvious).
+I have not found an obvious way to make a screen reader announce this situation to the user.
+There is no current ARIA specification that deals with this.
+*/
