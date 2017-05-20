e1303b45b20cdf477141385c8b1659aa3c0e1e99
Luis <luiscaceresmx@gmail.com>
2017-05-21 02:08:43 +1000

    Deleted StopWatch.js as API is no longer necessary. It was not a great idea.
    Polyfill for FocusEvent() is now aware of the blur, focusin and focusout events.
    Replaced var declarations for const declarations where appropriate.

--- components/polyfills/event constructors.js
+++ components/polyfills/event constructors.js
@@ -1,14 +1,48 @@
-/* Event constructors such as KeyboardEvent, MouseEvent and FocusEvent are not exposed as functions on Internet Explorer. The 'typeof' operator returns 'object' instead of 'function'. The opposite happens on the rest of the major browsers where event constructors are functions. This means that event constructors cannot be used with the 'new' keyboard on Internet Explorer. The following is a polyfill that converts event constructors into functions on Internet Explorer. This effectively aligns Internet Explorer with the rest of the major browsers. The following polyfill is expected to be used in conjunction with unit test files. */
+/* Event constructors such as KeyboardEvent, MouseEvent and FocusEvent are not exposed as functions on Internet Explorer 11. The 'typeof' operator returns 'object' instead of 'function'. The opposite happens on the rest of the major browsers where event constructors are functions. This means that event constructors cannot be used with the 'new' keyboard on Internet Explorer. The following is a polyfill that converts event constructors into functions on Internet Explorer. This effectively aligns Internet Explorer with the rest of the major browsers. The following polyfill is expected to be used in conjunction with unit test files. */
 
-if (typeof KeyboardEvent !== 'function') {
-    KeyboardEvent = function KeyboardEvent(type, eventInitDict) {
-        var event = document.createEvent('CustomEvent');
-        event.initCustomEvent(type, true, true, undefined); 
+(function () {
+    'use strict';
 
-        for (let key in eventInitDict) {
-            event[key] = eventInitDict[key];
+    if (typeof FocusEvent !== 'function') {
+        FocusEvent = function FocusEvent(type, eventInitDict) {
+            const EVENT = document.createEvent('CustomEvent');
+
+            /** @type {Boolean} */            
+            var canBubble;
+            switch (type) {
+                case 'blur':
+                case 'focus':
+                    canBubble = false;    
+                case 'focusout':
+                case 'focusin':
+                    canBubble = true;    
+            }
+
+            // Parameters are type, canBubble, cancelable, detail.
+            EVENT.initCustomEvent(type, canBubble, false, undefined);
+
+            for (let key in eventInitDict) {
+                EVENT[key] = eventInitDict[key];
+            }
+
+            return EVENT;
         }
-
-        return event;
     }
-}
+
+
+    if (typeof KeyboardEvent !== 'function') {
+        KeyboardEvent = function KeyboardEvent(type, eventInitDict) {
+            const EVENT = document.createEvent('CustomEvent');
+            // Parameters are type, canBubble, cancelable, detail.
+            EVENT.initCustomEvent(type, true, true, undefined);
+
+            for (let key in eventInitDict) {
+                EVENT[key] = eventInitDict[key];
+            }
+
+            return EVENT;
+        }
+    }
+ }());
+
+
