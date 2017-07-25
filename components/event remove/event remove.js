b9a6cbaa9cb6c3ec55fb0f2f9c2453ec8a4994f9
Luis <lcaceres@deloitte.com.au>
2017-07-25 17:52:11 +1000

    refactor: Change identifiers to align them with comments

--- components/event remove/event remove.js
+++ components/event remove/event remove.js
@@ -8,7 +8,7 @@ event called 'remove'. */
 (function () {
     "use strict";
 
-    // Let 'event' be a remove event.
+    // Let 'removeEvent' be a remove event.
     var removeEvent = new Event('remove');
 
     (function () {
@@ -18,6 +18,8 @@ event called 'remove'. */
          */
         function onPointerDown(event) {
             // Let 'target' be the target of the event.
+            const target = event.target;
+
             // If 'target' is a button able to remove its associated element.
             if (target.classList.contains('js-delete-button')) {
                 // Let 'button' be 'target'.
@@ -31,11 +33,11 @@ event called 'remove'. */
                 // Remove 'element'.
                 element.remove();
             }
-        });
-    })();
+        }
 
         // Listen for pointerdown events firing anywhere in the application.
         window.addEventListener('pointerdown', onPointerDown, true);
+    }());
 
 
     (function () {
@@ -46,28 +48,28 @@ event called 'remove'. */
          */
         function onFocusIn(event) {
             // Let 'target' be the target of the event.
-            var activeElement = document.activeElement,
-                id;
+            const target = event.target;
+            // Let 'ID' be the value of 'target's id attribute (if any).
+            const ID = target.id;
 
-            // If 'target' has a value for its id attribute.
-            if (id = activeElement.id) {
-                // Look for a button that is able to remove 'target'.
-                let deleteButton = document.querySelector('[aria-controls*=' + id + ']');
+            // If 'ID' is a value.
+            if (ID) {
+                // Let 'SELECTOR' be a CSS selector matching a button able to remove 'target'.
+                const SELECTOR = '[aria-controls*=' + ID + ']';
+                // Let 'match' be the element that 'SELECTOR' matches (if any).
+                const match = document.querySelector(SELECTOR);
 
-                // If there is a button able to remove 'target'.
-                // Let 'button' be the button able to remove 'target'.
-                if (deleteButton) {
+                // If 'match' is an element.
+                if (match) {
                     // Let 'element' be 'target'.
-                    let removee = activeElement;
-                    // Associate 'element' with 'target'.
-                    removee.deleteButton = deleteButton;
+                    const element = target;
                     // Listen for 'focusout' events originating at 'element'.
                     element.addEventListener('focusout', onFocusOut, true);
                     // Listen for 'keydown' events originating at 'element'.
-                    removee.addEventListener('keydown', keydownHandler);
+                    element.addEventListener('keydown', onKeyDown, true);
                 }
             }
-        }, true);
+        }
 
 
         /** Remove event handlers associated with an element that may be
@@ -77,12 +79,11 @@ event called 'remove'. */
         function onFocusOut(event) {
             /** @type {HTMLElement} */
             // Let 'element' be an HTML element that may be removed.
-            var removee = this;
+            const element = event.target;
             // Stop listening for 'focusout' events fired at 'element'.
             element.removeEventListener('focusout', onFocusOut, true);
             // Stop listening for 'keydown' events fired at 'element'.
-            removee.removeEventListener('keydown', keydownHandler);
-            event.stopPropagation();
+            element.removeEventListener('keydown', onKeyDown, true);
         };
 
 
@@ -90,22 +91,22 @@ event called 'remove'. */
          * @param {KeyboardEvent} event
          * @listens KeyboardEvent.type === 'keydown'
          */
-        var keydownHandler = function keydownHandler(event) {
-            /** @type {HTMLElement} */
-            // Let 'element' be the element to be removed.
-            var removee = this;
-
+        function onKeyDown(event) {
+            // Let 'KEY' be the key pressed.
+            const KEY = event.key;
             // If the 'DELETE' key was pressed.
-            if (event.keyCode === 46) {
+            if (KEY === 'Delete' || KEY === 'Del') {
+                /** @type {HTMLElement} */
+                // Let 'element' be the element to remove.
+                const element = event.target;
                 // Fire a remove event.
-                removee.dispatchEvent(removeEvent);
+                element.dispatchEvent(removeEvent);
                 // Remove 'element'.
-                removee.remove();
-                // Remove the delete button associated with 'element'.
-                removee.deleteButton.remove();
+                element.remove();
             }
         };
-    })();
 
         // Listen for focusin events originating anywhere in the application.
         window.addEventListener('focusin', onFocusIn, true);
+    }());
+}());
