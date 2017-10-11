69ec8517e87c10e1b7b76b35b66748c0b8f2fced
Luis <lcaceres@deloitte.com.au>
2017-10-11 15:01:08 +1100

    refactor: Add a static a server using plain vanilla JavaScript
    Before, the static server was enabled by the popular library Express for
    Node. This had the disavantage of depending on the existence of Express.
    Now, the static server is created with the use of plain vanilla
    JavaScript.

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-var express = require('express');
-var app = express();
+/* The following piece of code creates a static server for the entire project.
+It does not rely on any libraries. This is written in vanilla JavaScript for
+Node JS. In order to create the server go to the root folder and open the
+command line. Then type node server. You should be able to access any files on
+an Internet browser. */
 
-app.set('port', (process.env.PORT || 5001));
+// Let PORT be one of the supported ports on Safari for Browser Stack.
+const PORT = 8080;
 
-app.use(express.static(__dirname + '/components'));
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-// views is directory for all template files
-app.set('views', __dirname + '/input');
-app.set('view engine', 'jade');
+http.createServer(function (request, response) {
+  console.log('request ', request.url);
 
-app.get('*', function(request, response) {
-  var url = request.url;
-  if (url.indexOf('.') > -1 === false) {
-    console.log(url);
-    url = url.replace(/-/g, ' ');
-    response.sendFile(__dirname + `/components${url}/index.html`);
-  }
-  else {
-    console.log(url);
-  }
-});
+  var filePath = '.' + request.url;
+  if (filePath == './')
+    filePath = './index.html';
 
-app.get('/accessibility', function(request, response) {
-  response.render('pages/accessibility');
-});
+  var extname = String(path.extname(filePath)).toLowerCase();
+  var contentType = 'text/html';
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+  };
 
-app.get('/turtle', function(request, response) {
-  response.render('pages/turtle');
-});
+  contentType = mimeTypes[extname] || 'application/octet-stream';
 
-app.get('/two-dimensional-navigation', function(request, response) {
-  response.render('pages/two-dimensional-navigation');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
-
-
-/* The purpose of the following piece of code is to eliminate the need of manually 
-assigning values to 'id' attributes. This is especially true when the value is 
-absolutely meaningless and does not convey any information about the application 
-to the user. */
-function* IDGenerator() {
-    var number = 0;
-    while (true) {
-        yield "a" + number++;
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('./404.html', function (error, content) {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+        response.end();
+      }
     }
-}
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
 
-global.IDGenerator = IDGenerator();
+}).listen(PORT);
+console.log('Server running at http://127.0.0.1:8080/');
